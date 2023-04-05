0001ae8cd2327e70d32e08f1ea72f80d0627158b
František Čech <frantisek.cech@nanoenergies.cz>
2023-04-05 21:46:00 +0200

    store init

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppShell from "./AppShell/Appshell";
-import Trash from "./Pages/trash/Trash";
+import Trash from "./pages/trash/Trash";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+
+export const store = configureStore({
+  reducer: {}
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppShell>
-        <Routes>
-          <Route path="" element={<Trash />}></Route>
-        </Routes>
-      </AppShell>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppShell>
+          <Routes>
+            <Route path="/" element={<Trash />}></Route>
+          </Routes>
+        </AppShell>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
