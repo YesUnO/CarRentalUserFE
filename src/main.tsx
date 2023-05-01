43655d8814622d1aee54b77a61234d5abe0c50e5
František Čech <frantisek.cech@nanoenergies.cz>
2023-05-01 17:42:22 +0200

    protected admin routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppShell from "./AppShell/Appshell";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from 'react-redux';
-import store from './infrastructure/store';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Provider, useSelector } from 'react-redux';
+import store, { RootState } from './infrastructure/store';
 import UserPage from "./features/User/UserPage";
 import AdminPage from "./features/Admin/AdminPage";
 import PickCarPage from "./features/Car/PickCarPage/PickCarPage";
@@ -12,20 +12,25 @@ import OrderDetailPage from "./features/Order/OrderDetailPage";
 import AdminUserManagmentPage from "./features/Admin/User/AdminUserManagmentPage/AdminUserManagmentPage";
 
 
+const Root: React.FC = () => {
+  const isAdmin = useSelector((state: RootState) => state.authService.role == "Admin");
+  return (<BrowserRouter>
+    <AppShell>
+      <Routes>
+        <Route path="/" element={<PickCarPage />} />
+        <Route path="/orderDetail" element={<OrderDetailPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/admin" element={isAdmin ? <AdminPage /> : <Navigate to={"/"} />} />
+        <Route path="/admin/user" element={isAdmin ? <AdminUserManagmentPage /> : <Navigate to={"/"} />} />
+      </Routes>
+    </AppShell>
+  </BrowserRouter>)
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AppShell>
-          <Routes>
-            <Route path="/" element={<PickCarPage />}></Route>
-            <Route path="/orderDetail" element={<OrderDetailPage />}></Route>
-            <Route path="/user" element={<UserPage />}></Route>
-            <Route path="/admin" element={<AdminPage />}></Route>
-            <Route path="/admin/user" element={<AdminUserManagmentPage />}></Route>
-          </Routes>
-        </AppShell>
-      </BrowserRouter>
+      <Root />
     </Provider>
   </React.StrictMode>
 );
