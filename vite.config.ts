252f674a4466b16d549ed15a698fe34b958d5b7a
František Čech <frantisek.cech@nanoenergies.cz>
2023-06-10 15:44:48 +0200

    https dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import EnvironmentPlugin from 'vite-plugin-environment'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    basicSsl(),
     EnvironmentPlugin(['API_URL','STRIPE_PRODUCT_ID','GOOGLE_CLIENT_ID']),
   ],
   server: {
+    port:5173,
+    strictPort:true,
     proxy: {
       "/bff": {
-        target: "https://localhost:7025",
+        target: "https://localhost:5001",
         secure: false,
       },
       "/signin-oidc": {
-        target: "https://localhost:7025",
+        target: "https://localhost:5001",
         secure: false,
       },
       "/signout-callback-oidc": {
-        target: "https://localhost:7025",
+        target: "https://localhost:5001",
         secure: false,
       },
     },
