31ad54d8af60bb5d2bb23ec1dcf4152c8e2ec027
František Čech <frantisek.cech@nanoenergies.cz>
2023-06-26 18:42:17 +0200

    mail confirm

--- src/features/User/Pages/EmailConfirmationPage.tsx
+++ src/features/User/Pages/EmailConfirmationPage.tsx
@@ -1,19 +1,29 @@
-import { useDispatch } from "react-redux";
-import { Button } from "antd";
-import { useNavigate } from "react-router-dom";
+import { Button, message } from "antd";
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 
 const EmailConfirmationPage: React.FC = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const msg = searchParams.get("result");
+    const [message, setMessage] = useState("Thank you for confirming your email.")
 
-
+    useEffect(()=>{
+        if (msg == "False") {
+            setMessage("Wrong");
+        }
+        if (msg == "Resended") {
+            console.log("yo");
+        }
+    },[msg])
+    
     const handleLogin = () => {
-        navigate("/user");
+        window.location.href = "/bff/login?returnUrl=/";
     };
 
     return(
         <>
-        <div>Thank you for confirming your email. <Button type="link" onClick={handleLogin}>Sign in</Button> </div>
+        <div>{message} <Button type="link" onClick={handleLogin}>Sign in</Button> </div>
         </>
     );
 };
